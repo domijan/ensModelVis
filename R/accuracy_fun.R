020a11e9c614dce158b1180ee0b1e74a386c7a4e
domijan <domijank@tcd.ie>
2022-06-24 23:24:44 +0100

    removed docu

--- R/accuracy_fun.R
+++ R/accuracy_fun.R
@@ -1,12 +1,4 @@
-#' Title
-#'
-#' @param truth
-#' @param pred
-#'
-#' @return
-#' @export
-#'
-#' @examples
+
 accuracy_fun <- function(truth, pred){
   sum(diag(table(truth, pred)))/sum(table(truth, pred))
 }
